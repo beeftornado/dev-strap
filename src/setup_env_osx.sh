8698c9605a696e96b76d5b418ad65f8e8d895a32
Casey <casey.duquette@gmail.com>
2014-05-24 00:22:07 -0700

    chg: cleanup

--- src/setup_env_osx.sh
+++ src/setup_env_osx.sh
@@ -12,45 +12,104 @@
 #     try mount -o remount,rw /boot
 #     next
 
-echo
+clear
 
-confirm "Do you want python setup and configured (python, pip, virtualenv)?"
-SETUP_PYTHON=$?
+# OSX version requirement
+if [[ ! $(sw_vers -productVersion | egrep '10.[89]')  ]]
+then
+  echo "This script is only certified for OSX versions 10.8 (Mountain Lion) and 10.9 (Mavericks), aborting..."
+  exit 1
+fi
 
-confirm "Do you want nodejs setup and configured (node, nvm)?"
-SETUP_NODEJS=$?
-
-confirm "Do you want ruby setup and configured (ruby, rvm)?"
-SETUP_RUBY=$?
-
-confirm "Do you want database support (mysql, mongo)?"
-SETUP_DB=$?
-
-#confirm "Do you want to install the list of suggested homebrew apps? (see Brewfile)"
-#SETUP_BREWFILE=$?
-
-echo
+# Disk space requirement
+df -H / | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 90 ]; then
+    echo "Running out of space \"$partition ($usep%)\" on $(hostname) on $(date)"
+    echo "Aborting..."
+    exit 1
+  fi
+done
 
 step "Installing homebrew: "
 if [ ! -f /usr/local/bin/brew ]; then
   try ruby -e "$(\curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  next
+else
+  skip
 fi
-next
 
-#step "Checking homebrew: "
-#try brew doctor > /dev/null
-#try brew prune > /dev/null
-#next
+step "Installing whiptail (pretty menu): "
+if [ ! -f /usr/local/bin/whiptail ]; then
+  try brew install newt
+  next
+else
+  skip
+fi
+
+menu=(whiptail --separate-output --title "Install Options" --checklist "Select the dev options you want (I recommend having all):" 0 0 0)
+options=(1 "Python, pip, virtualenv" on
+        2 "NodeJS, NVM" on
+        3 "Ruby, RVM" on
+        4 "Mysql, Mongo" on
+        5 "Common dependencies from Homebrew" on
+        6 "Development tools (apps like editors)" on
+        7 "Additional apps everyone should have (like chrome)" on)
+choices=$("${menu[@]}" "${options[@]}" 2>&1 > /dev/tty)
+
+if [[ $? -ne 0 ]]; then
+  echo "Aborting..."
+  exit 1
+fi
+
+choice_count=$(echo "$choices" | grep -v '^$' | wc -l)
+if [ $choice_count -eq 0 ]; then
+  echo "Nothing selected."
+  exit 0
+fi
+
+for choice in $choices
+do
+  case $choice in
+  1)
+      SETUP_PYTHON=0
+  ;;
+  2)
+      SETUP_NODEJS=0
+  ;;
+  3)
+      SETUP_RUBY=0
+  ;;
+  4)
+      SETUP_DB=0
+  ;;
+  5)
+      SETUP_HOMEBREW_COMMONS=0
+  ;;
+  6)
+      SETUP_DEV_APPS=0
+  ;;
+  7)
+      SETUP_COMMON_APPS=0
+  ;;
+  esac
+done
+
 
 step "Bootstrapping homebrew: "
 egrep "PATH.*/usr/local/bin" ~/.bashrc >/dev/null
 if [[ $? -ne 0 ]]; then
   try echo 'export PATH=/usr/local/bin:$PATH' >> ~/.bashrc
+  next
+else
+  skip
 fi
-next
 
 step "Installing python: "
-if [[ $SETUP_PYTHON -eq 1 ]]; then
+if [[ $SETUP_PYTHON ]]; then
     try brew install python > /dev/null 2>/dev/null
     PYTHON_VERSION=$(python -V 2>&1 | /usr/bin/awk -F' ' '{print $2}')
     vercomp $PYTHON_VERSION "2.7"
@@ -64,7 +123,7 @@ else
 fi
 
 step "Installing pip: "
-if [[ $SETUP_PYTHON -eq 1 ]]; then
+if [[ $SETUP_PYTHON ]]; then
     if [ ! -f /usr/local/bin/pip ]; then
       try sudo easy_install pip > /dev/null 2>/dev/null
     fi
@@ -83,11 +142,13 @@ step "Installing gevent dependency libevent: "
 brew list libevent > /dev/null 2>/dev/null
 if [[ $? -ne 0 ]]; then
   try brew install libevent > /dev/null 2>/dev/null
+  next
+else
+  skip
 fi
-next
 
 step "Installing mysql: "
-if [[ $SETUP_DB -eq 1 ]]; then
+if [[ $SETUP_DB ]]; then
     brew list mysql > /dev/null 2>/dev/null
     if [[ $? -ne 0 ]]; then
       try brew install mysql > /dev/null 2>/dev/null
@@ -98,7 +159,7 @@ else
 fi
 
 step "Installing mongodb: "
-if [[ $SETUP_DB -eq 1 ]]; then
+if [[ $SETUP_DB ]]; then
     brew list mongodb > /dev/null 2>/dev/null
     if [[ $? -ne 0 ]]; then
       try brew install mongodb > /dev/null 2>/dev/null
@@ -109,7 +170,7 @@ else
 fi
 
 step "Installing node version manager: "
-if [[ $SETUP_NODEJS -eq 1 ]]; then
+if [[ $SETUP_NODEJS ]]; then
     nvm --version > /dev/null 2>/dev/null
     if [[ $? -ne 0 ]]; then
         # missing nvm
@@ -130,16 +191,22 @@ else
 fi
 
 step "Installing nodejs v0.10: "
-if [[ $SETUP_NODEJS -eq 1 ]]; then
+if [[ $SETUP_NODEJS ]]; then
     try nvm install 0.10 > /dev/null 2>/dev/null
     try nvm alias default v0.10 >/dev/null 2>/dev/null
+
+    egrep "PATH.*/usr/local/share/npm/bin" ~/.bashrc >/dev/null
+    if [[ $? -ne 0 ]]; then
+      try echo 'export PATH=/usr/local/share/npm/bin:$PATH' >> ~/.bashrc
+    fi
+
     next
 else
     skip
 fi
 
 step "Installing python virtual env: "
-if [[ $SETUP_PYTHON -eq 1 ]]; then
+if [[ $SETUP_PYTHON ]]; then
     try pip install virtualenv virtualenvwrapper > /dev/null 2>/dev/null
     next
 else
@@ -147,7 +214,7 @@ else
 fi
 
 step "Bootstrapping virtualenvwrapper: "
-if [[ $SETUP_PYTHON -eq 1 ]]; then
+if [[ $SETUP_PYTHON ]]; then
     # Find the virtualenvwrapper shell script
     if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
       VE_WRAPPER_LOC="/usr/local/bin/virtualenvwrapper.sh"
@@ -156,11 +223,6 @@ if [[ $SETUP_PYTHON -eq 1 ]]; then
       VE_WRAPPER_LOC="/usr/local/share/python/virtualenvwrapper.sh"
     fi
     try source $VE_WRAPPER_LOC > /dev/null 2>/dev/null
-
-    egrep "PATH.*/usr/local/share/npm/bin" ~/.bashrc >/dev/null
-    if [[ $? -ne 0 ]]; then
-      try echo 'export PATH=/usr/local/share/npm/bin:$PATH' >> ~/.bashrc
-    fi
 
     egrep "export VIRTUALENVWRAPPER_PYTHON=" ~/.bashrc >/dev/null
     if [[ $? -ne 0 ]]; then
@@ -177,7 +239,7 @@ else
 fi
 
 step "Installing ruby version manager: "
-if [[ $SETUP_RUBY -eq 1 ]]; then
+if [[ $SETUP_RUBY ]]; then
     RVM_PATH=$(echo $rvm_path 2>&1)
     if [ -e $RVM_PATH ]; then
         # User already has rvm
@@ -197,11 +259,15 @@ else
 fi
 
 step "Installing ruby: "
-if [[ $SETUP_RUBY -eq 1 ]]; then
-    try $RVM_PATH/bin/rvm install 1.9.2 > /dev/null 2>/tmp/ruby-install.err
-    if [[ $? -ne 0 ]]; then
-        cat /tmp/ruby-install.err
-        rm /tmp/ruby-install.err
+if [[ $SETUP_RUBY ]]; then
+    RUBY_VER=$(ruby -v | awk -F' ' '{print $2}' | awk -F'p' '{print $1}')
+    vercomp "$RUBY_VER" 1.9.2
+    if [ $? -eq 2 ]; then
+      try $RVM_PATH/bin/rvm install 1.9.2 > /dev/null 2>/tmp/ruby-install.err
+      if [[ $? -ne 0 ]]; then
+          cat /tmp/ruby-install.err
+          rm /tmp/ruby-install.err
+      fi
     fi
     next
 else
@@ -209,36 +275,3 @@ else
 fi
 
 exit 0
-
-echo -n "Type the name for your python virtual environment followed by [ENTER]:"
-read vname
-
-mkvirtualenv "$vname"
-workon "$vname"
-
-if [ -f requirements.txt ]; then
-  # install required python modules
-  pip install -r requirements.txt -i https://pypi.prod.hulu.com/simple
-fi
-if [ -f src/requirements.txt ]; then
-  # install required python modules
-  pip install -r src/requirements.txt -i https://pypi.prod.hulu.com/simple
-fi
-
-if [ -f package.json ]; then
-  # install node.js and npm package manager (for node.js packages)
-  npm install .   # if there's a package.json file
-else
-  # ...and if not
-  npm install coffee-script@1.4.0 -g
-  npm install less -g
-  npm install uglify-js -g
-fi
-
-echo ""
-echo "To work on this project in the future, you will need to run:"
-echo ""
-echo "workon $vname"
-echo ""
-
-echo "DONE!"
